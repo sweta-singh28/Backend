83af358d40d1c96e4298215950efdd9ab5a4bb10
sweta-singh28 <swetasinghgs28@gmail.com>
2025-01-13 21:35:04 +0530

    sweta

--- Native Modules/index.js
+++ Native Modules/index.js
@@ -3,9 +3,13 @@
 const fs = require("fs");
 
 //creating a file
-fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
-    if(err)throw err;
-    console.log("The file has been saved!");
-});
+//fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+  //  if(err)throw err;
+    //console.log("The file has been saved!");
+//});
 
 //reading message from a file
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
