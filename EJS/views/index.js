6c9c4586e6974ee124da70169a431a995f96d30d
sweta-singh28 <swetasinghgs28@gmail.com>
2025-01-17 23:35:46 +0530

    sweta

---
+++ EJS/views/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs", {
+        dataType: "a weekday",
+        advice: "it's time to work hard",
+    });
+});
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}.`)
+})
